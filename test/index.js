28a4a6a506b92d3ec386c9b42d389733ae3882e2
ZaeemTarrar <zaeemtarrar3@gmail.com>
2021-09-30 13:19:56 +0500

    new updates

--- test/index.js
+++ test/index.js
@@ -1,3 +1,15 @@
-const caps = (text) => text[0].toUpperCase() + text.slice(1).toLowerCase();
+const abc = () => {
+	new Promise((resolve, reject) => resolve(new Audio('beep-1.mp3')))
+		.then((audio) => {
+			audio.autoplay = false;
+			audio.loop = false;
+			audio.muted = false;
+			audio.volume = 0.2;
+			audio.playbackRate = 7;
+			return audio;
+		})
+		.then((audio) => audio.play())
+		.catch((err) => console.error('Error: ', err));
+};
 
-console.log(caps(word));
+abc();
